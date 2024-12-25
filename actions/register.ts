d2088cc5ac690a53038c3cf607b4a737515c7b02
Rezi Karanadze <karanadzerevazi@gmail.com>
2024-12-25 14:55:31 +0400

    implement register functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
+import { db } from "@/lib/db";
 import { registerSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const validatedFields = registerSchema.safeParse(values);
@@ -13,7 +16,29 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
     };
   }
 
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send email to user
+
   return {
-    success: "Email sent",
+    success: "User created!",
   };
 };
